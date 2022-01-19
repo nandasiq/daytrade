2a4e72fa401105e20f38ac11022d01a1d75051e5
Guilherme Andrade e Chaves <guilherme@live.it>
2022-01-19 02:49:53 -0300

    Alteração button logout para NavBar | Alteração geral CSS

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext}  from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import StoreContext from '../Store/Context';
+import './style.css'
+
 
 const Header = () => {
+    const { setToken } = useContext(StoreContext);
     return (
 
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -10,9 +14,9 @@ const Header = () => {
                 <Navbar.Brand href="#home">Trade diary AFT</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        Conectado como: <a href="#login">Guilherme Andrade</a>
-                    </Navbar.Text>
+                        <button className="button-logout" type="button" onClick={() => setToken(null)}>
+                          Sair
+                        </button>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,14 @@
-import React, { useContext} from 'react';
+import React from 'react';
 import Header  from '../../components/NavBar';
 import Drop  from './components/Drop';
 import Footer  from '../../components/Footer';
-import StoreContext from '../../components/Store/Context';
 
 
 
 const Home = () => {
-  const { setToken } = useContext(StoreContext);
-
   return (
     <>
     <Header />
-    <button type="button" onClick={() => setToken(null)}>
-        Sair
-    </button>
     <Drop />
     <Footer />
     </>
